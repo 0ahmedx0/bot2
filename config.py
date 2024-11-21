5960fc3c31dc71989f21d3e4fd9eb917fca6f5b4
0ahmedx0 <144847462+0ahmedx0@users.noreply.github.com>
2024-11-21 23:05:09 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,13 @@
-from os import environ 
 
-class Config:
-    API_ID = environ.get("API_ID", "27361100")
-    API_HASH = environ.get("API_HASH", "70f07944c80e1e52784f14cfe49f37fa")
-    BOT_TOKEN = environ.get("BOT_TOKEN", "7822077170:AAHpdjQIfh2SZl0wbx_Seb8RtTlWE7FbVlg") 
-    BOT_SESSION = environ.get("BOT_SESSION", "bot") 
-    DATABASE_URI = environ.get("DATABASE", "mongodb://ahmedalsaltani30:ahmedabcd074@cluster0-shard-00-00.quu8v.mongodb.net:27017,cluster0-shard-00-01.quu8v.mongodb.net:27017,cluster0-shard-00-02.quu8v.mongodb.net:27017/?ssl=true&replicaSet=atlas-s98j2a-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0")
-    DATABASE_NAME = environ.get("DATABASE_NAME", "forward-bot")
-    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '6419499019').split()]
+# safe_repo
+# Note if you are trying to deploy on vps then directly fill values in ("")
 
-class temp(object): 
-    lock = {}
-    CANCEL = {}
-    forwardings = 0
-    BANNED_USERS = []
-    IS_FRWD_CHAT = []
+from os import getenv
+
+API_ID = int(getenv("API_ID", "27361100"))
+API_HASH = getenv("API_HASH", "70f07944c80e1e52784f14cfe49f37fa")
+BOT_TOKEN = getenv("BOT_TOKEN", "7822077170:AAHpdjQIfh2SZl0wbx_Seb8RtTlWE7FbVlg")
+OWNER_ID = list(map(int, getenv("OWNER_ID", "6419499019").split()))
+MONGO_DB = getenv("MONGO_DB", "mongodb://ahmedalsaltani30:ahmedabcd074@cluster0-shard-00-00.quu8v.mongodb.net:27017,cluster0-shard-00-01.quu8v.mongodb.net:27017,cluster0-shard-00-02.quu8v.mongodb.net:27017/?ssl=true&replicaSet=atlas-s98j2a-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0")
+LOG_GROUP = getenv("LOG_GROUP", "-1002293967199")
+CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002293967199"))
